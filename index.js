1873d6bd3416292aa626255c6a62c8c81f0b66ea
Christopher Anderson <andersonchris227@gmail.com>
2018-09-19 14:55:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeliLine = [];
 var otherDeli = ["Steven", "Blake", "Avi"];
 
 function takeANumber() {
-  console.log ('Welcome, Ada. You are number 1 in line.');
+  console.log ('Welcome, +name+. You are number 1 in line.');
 }
